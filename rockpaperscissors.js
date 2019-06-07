170eafec999593020fb74c595aa7ef1cbaf4b53b
Adam Essmyer <acemyer@gmail.com>
2019-06-07 08:26:22 -0500

    added functions for rps

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,34 @@
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
+let computerSelection;
+let playerSelection;
+
+const buttons = document.querySelectorAll('button');
+const scoreBoard = document.querySelector('.scoreBoard');
+
+function computerPlay() {
+    let choice = Math.floor(Math.random() * 3);
+
+    switch (choice) {
+        case 0: return computerSelection = "rock";
+        break;
+        case 1: return computerSelection = "paper";
+        break;
+        case 2: return computerSelection = "scissors";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = buttonID;
     return compareSelection(playerSelection, computerPlay());
@@ -5,4 +36,4 @@ function playRound(playerSelection, computerSelection) {
 
 const playerSelection = 'rock';
 const computerSelection = computerPlay(
-console.log(playRound(playerSelection,computerSelection));
+console.log(playRound(playerSelection,computerSelection)));
