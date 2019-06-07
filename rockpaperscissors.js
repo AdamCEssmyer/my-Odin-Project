be87c8d80dd3e5937fe0b201775bccbb866f083f
Adam Essmyer <acemyer@gmail.com>
2019-06-07 08:18:42 -0500

    created js file 'rock paper scissors'

---
+++ rockpaperscissors.js
@@ -0,0 +1,8 @@
+function playRound(playerSelection, computerSelection) {
+    playerSelection = buttonID;
+    return compareSelection(playerSelection, computerPlay());
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay(
+console.log(playRound(playerSelection,computerSelection));
