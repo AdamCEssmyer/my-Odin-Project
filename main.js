fc68e662c638665160d261dca06921cacc24c8b4
Adam Essmyer <acemyer@gmail.com>
2020-05-12 13:11:39 -0500

    changed js file name

---
+++ main.js
@@ -0,0 +1,11 @@
+function playRound(playerSelection, computerSelection) {
+    switch (Math.floor(math.random() * 3)) {
+        case 0: 
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+        
+    }
+}
