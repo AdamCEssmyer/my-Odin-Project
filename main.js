0ac051cec58528b90fe7bce34d940e864d242808
Adam Essmyer <acemyer@gmail.com>
2020-05-12 13:34:17 -0500

    created friends List script

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-function playRound(playerSelection, computerSelection) {
-    switch (Math.floor(math.random() * 3)) {
-        case 0: 
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors';
-        
+var friendsList = {
+    steve: {
+        firstName: "Steve",
+        lastName: "Dude",
+        email: "steve@email.com"
+    },
+
+    john: {
+        firstName: "John",
+        lastName: "Johnson",
+        email: "jjohnson@email.com"
     }
-}
+};
+
+var list = function(object)
+{
+    for(var key in object){
+        console.log(key);
+    }
+};
+
+var search = function(name) {
+    for (var prop in friendsList) {
+        if(friendsList[prop].firstName.toLowerCase() === name){
+            console.log(friendsList[prop]);
+            return friendsName;
+        }
+    }
+};
